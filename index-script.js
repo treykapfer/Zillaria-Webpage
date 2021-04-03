75127d589865ad2cac8dcb3df844e196ed72d11e
Trey K <80800860+treykapfer@users.noreply.github.com>
2021-04-03 12:56:27 -0500

    javaupdate

--- index-script.js
+++ index-script.js
@@ -1,3 +1,35 @@
-function hoverDisplayThauglit () {
-    document.getElementById("thauglitHover").style.display = "block";
+var setMarker = 0;
+
+function open_boxchild(clicked_id)     {
+        var currentId = document.getElementById(clicked_id);
+        var currentHead = currentId.getElementsByTagName("h3")[0];
+        var currentPar = currentId.getElementsByTagName("p")[0];
+        var currentLink = currentId.getElementsByTagName("a")[0];
+        
+        //add inline styles
+        currentPar.style.removeProperty("display");
+        currentLink.style.removeProperty("display");
+        currentId.style.margin = "1.5rem .5rem";
+        currentId.style.position = "sticky";
+        currentHead.style.fontSize = "1.75rem";
+        currentId.style.order = "0";
+
+        if (setMarker == 1) {
+            setMarker--;
+            
+            //clear inline styles//
+            currentId.style.removeProperty("order");
+            currentPar.style.display = "none";
+            currentLink.style.display = "none";
+            currentId.style.removeProperty("margin");
+            currentId.style.removeProperty("position");
+            currentHead.style.removeProperty("font-size");
+
+            console.log(setMarker);
+            } else if (setMarker == 0) {
+                setMarker++;
+            } else {
+                alert("An error has occurred. Please refresh the page.");
+            }
+
 }
